9f6c69ed82b7a8e4b997e53d3ccdcbba9effe90b
Hiago 7k <hiago7k@gmail.com>
2026-04-07 19:31:19 -0300

    desafio

---
+++ Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praticce_Serelizar_x_Deserealizer;
+
+internal class Pessoa
+{
+    public Pessoa(string nome, int idade, string email)
+    {
+        Nome = nome;
+        Idade = idade;
+        Email = email;
+    }
+
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Email { get; set; }
+}
